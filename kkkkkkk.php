bcbb15e799da118c470bdb7b424bab7718205537
ilona65652 <162963291+ilona65652@users.noreply.github.com>
2024-03-18 11:19:00 +0600

    Add files via upload

---
+++ kkkkkkk.php
@@ -0,0 +1,12 @@
+<?php
+// Опение функции длячисления снего значения
+ calculateAverage($numbers) {
+    $sum =_sum($numbers);
+    $ = count($numbers    $average = $sum / $count;
+    return $average;
+}
+
+// Пример использования функции
+$numbers array(10, 20, 30, 40, 50);
+echo "Среднее значение: " . calculateAverage($numbers);
+?>
